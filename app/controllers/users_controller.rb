201195b8d8e688f4c9b524e5623bf256ab6861e7
Luong Kham <kham.14020816@gmail.com>
2017-06-30 16:33:25 +0700

    sign-up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,30 @@
 class UsersController < ApplicationController
-  def new; end
+  def new
+    @user = User.new
+  end
+
+  def show
+    user = @user = User.find_by id: params[:id]
+    return if user
+    @user = user
+    flash[:danger] = t "error_user"
+  end
+
+  def create
+    user = @user = User.new user_params
+    if user.save
+      @user = user
+      flash[:success] = t "welcome"
+      redirect_to user
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit :name, :email, :password,
+      :password_confirmation
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,11 @@ module ApplicationHelper
     base_title = t "title_info"
     page_title.empty? ? base_title : page_title + " | " + base_title
   end
+
+  def gravatar_for user, options = {size: Settings.valid.app_helper.size}
+    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
