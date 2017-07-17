e7899f479f575a8dbebf9e98c54ffe127363e54f
Luong Kham <kham.14020816@gmail.com>
2017-07-17 14:27:00 +0700

    account activated

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,11 @@
+class UserMailer < ApplicationMailer
+  def account_activation user
+    @user = user
+    mail to: user.email, subject: t("Account_activation")
+  end
+
+  def password_reset
+    @greeting = t "hi"
+    mail to: t "email_example"
+  end
+end
